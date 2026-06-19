65b1f3cbb8e5e0f2d71ba03707260d027a1f3c75
Omar <devumar0@gmail.com>
2026-06-19 16:57:22 +0300

    refacotring and mentance for the nextprayer and show time functions

--- app.js
+++ app.js
@@ -1,45 +1,65 @@
 
+function print(string)
+{
+  console.log(string);
+}
 
+// function GetTheNextPrayer(extend){
+
+//     const prayers = ["Fajr","Sunrise","Dhuhr","Asr","Maghrib","Isha"];
+   
+//     for (let index = 0; index < prayers.length; index++) {
+//       const element = prayers[index];
+//       if( == element)
+//         {
+//             return element;
+//         }
+//     }
+// }
 
 const time = new Date();
 
-time.toLocaleTimeString('en-US',{
+function showtime()
+{
+  time.toLocaleTimeString('en-US',{
         minute:'2-digit',
         second:'2-digit',
-        hour:'2-digit'
+        hour:'2-digit',
+        hour12:true
     });
+  
 
     let hours = time.getHours();
     let minutes = time.getMinutes();
     let seconds = time.getSeconds();
     let LocalTime = hours + ':' + minutes + ':' + seconds ;
-    
 
+    document.getElementById("Local-Time").innerText = LocalTime;
+    console.log(LocalTime);
 
-
-
-
-
-
-
-
-
-
-
-let params = {
-    
-    date: "18-6-2026",
-    city: "Jeddah",
-    country: "SA"
 }
 
+function NextPrayer()
+{
 
-axios.get("https://api.aladhan.com/v1/timingsByCity",
+let todayDate = time.getDay() + '-' + time.getMonth() + '-' + time.getFullYear();
+
+
+  let params = {
+    
+    date: todayDate,
+    latitude: 21.5435,
+    longitude: 39.16
+};
+
+
+axios.get("https://api.aladhan.com/v1/nextPrayer",
     {params: params})
   .then((response) => {
 
-    let Timings = response.data.data.timings;
-    console.log(response.data.data);
+    const Timings = response.data.data.timings;
+    document.getElementById("Next-Prayer").innerHTML = Timings;
+    console.log(Timings);
   })
   .catch((error) => {
     console.error(error);
@@ -49,11 +69,45 @@ axios.get("https://api.aladhan.com/v1/timingsByCity",
   });
 
 
+
+}
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // this is for the local time //
 
-document.getElementById("Local-Time").innerHTML = LocalTime;
 
-document.getElementById("Next-Prayer").innerHTML = Timings.Fajr;
+
+
+
+window.onload = function () {
+
+  setInterval(showtime, 500); 
+}
+
+window.onload = function () {
+
+  setInterval(NextPrayer,1000000);   // 3,600,000 = 1hour // 1,800,000 = half hour;
+}
+
 
 
 
